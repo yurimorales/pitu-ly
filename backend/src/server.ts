c2af0beadf6614abe7b03bed8560d653a0d0d90a
yurimorales19@gmail.com <yurimorales19@gmail.com>
2020-11-24 20:45:12 -0300

    Adicionada dependencia cors, pra comunicar front com o back-end. Alterada porta da 300 p/ 3001 do server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -7,5 +7,5 @@ import database from './database';
 database.sync({force: false});
 console.log('MySQL database running at port 3306');
 
-app.listen(3000); // Escuta o server na porta 3000
+app.listen(3001); // Escuta o server na porta 3000
 console.log('Server running at port 3000');
