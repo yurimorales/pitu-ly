da7642f2ec94474d7efd2ff6455582a09db985d9
yurimorales19@gmail.com <yurimorales19@gmail.com>
2020-11-25 20:08:01 -0300

    finalizado front-end

--- frontend/src/pages/RedirectPage/index.js
+++ frontend/src/pages/RedirectPage/index.js
@@ -1,14 +1,59 @@
 import React from 'react';
+import Header from '../../components/Header';
+import { Container } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { StatsContainer } from './styles';
+import ShortenerService from '../../services/shortenerService';
 
 class RedirectPage extends React.Component {
 
     constructor(props){
         super(props);
+
+        this.state = {
+            isLoading: false,
+            url: '',
+            errorMessage: ''
+        }
     }
 
-    render(){
+    async componentDidMount() {
+        const { code } = this.props.match.params;
+
+        try {
+
+            const service = new ShortenerService();
+            const { url } = await service.getLink(code);
+
+            window.location = url;
+
+        } catch (e) {
+            this.setState({ isLoading: false, errorMessage: 'Ops, a URL solicitada não existe :(' });
+        }
+
+    }
+
+    render() {
+
+        const { errorMessage } = this.state;
+
         return (
-            <p>Redirect</p>
+            <Container>
+                {errorMessage ? (
+                    <>
+                        <Header>
+                            Seu novo encurtador de URL :)
+                        </Header>
+                        <StatsContainer className="text-center">
+                            <FontAwesomeIcon size="3x" color="#f8d7da" icon="exclamation-triangle" />
+                            <p className="mb-3 mt-1">{errorMessage}</p>
+                            <a className="btn btn-primary" href="/">Encurtar nova URL</a>
+                        </StatsContainer>
+                    </>
+                ) : (
+                    <p className="text-center">Redirecionando...</p>
+                )}
+            </Container>
         )
     }
 
--- frontend/src/pages/StatsPage/index.js
+++ frontend/src/pages/StatsPage/index.js
@@ -4,6 +4,8 @@ import {Container} from 'react-bootstrap';
 import ShortenerService from '../../services/shortenerService';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { StatsContainer, StatsRow, StatsBox, StatsBoxTitle } from './styles';
+import {parseISO, formatRelative} from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 
 class StatsPage extends React.Component {
 
@@ -12,27 +14,67 @@ class StatsPage extends React.Component {
 
         this.state = {
             isLoading: false,
-            shortenerURL: {},
+            shortenedURL: {},
             errorMessage: ''
+        }
+    }
+
+    async componentDidMount() {
+
+        const { code } = this.props.match.params;
+        
+        try {
+
+            const service = new ShortenerService();
+            const shortenedURL = await service.getStats(code);
+
+            const parsedDate = parseISO(shortenedURL.updatedAt);
+            const currentDate = new Date();
+            
+            const relativeDate = formatRelative(parsedDate, currentDate, {
+                locale: ptBR,
+            });
+
+            shortenedURL.relativeDate = relativeDate;
+
+            this.setState({ isLoading: false, shortenedURL: shortenedURL, errorMessage: '' });
+
+        } catch(e) {
+            //console.log( e );
+            this.setState({ isLoading: false, errorMessage: 'Ops a URL não existe. Tente novamente mais tarde :(' });
         }
 
     }
 
     render(){
 
-        const {errorMessage, shortenerURL} = this.state;
+        const {errorMessage, shortenedURL} = this.state;
 
         return (
             <Container>
                 <Header>Estatísticas</Header>
                 {errorMessage ? (
-                    <StatsContainer>
-                        <FontAwesomeIcon sice="3x" color="#f8d7da" icon="{exclamation-triangle}" />
-                        <p className="mb-3">{errorMessage}</p>
+                    <StatsContainer className="text-center">
+                        <FontAwesomeIcon size="3x" color="#f8d7da" icon="exclamation-triangle" />
+                        <p className="mb-3 mt-1">{errorMessage}</p>
                         <a className="btn btn-primary" href="/">Encurtar nova URL</a>
                     </StatsContainer>
                 ) : (
-                    <p>Resultado</p>
+                    <StatsContainer className="text-center">
+                        <p><strong>http://pitu.io/{shortenedURL.code}</strong></p>
+                        <p>Redireciona para: <br/>{shortenedURL.url}</p>
+                        <StatsRow>
+                            <StatsBox>
+                                <b>{shortenedURL.hits}</b>
+                                <StatsBoxTitle>Visitas</StatsBoxTitle>
+                            </StatsBox>
+                            <StatsBox>
+                                <b>{shortenedURL.relativeDate}</b>
+                                <StatsBoxTitle>Última visita</StatsBoxTitle>
+                            </StatsBox>
+                        </StatsRow>
+                        <a className="btn btn-primary" href="/">Encurtar nova URL</a>
+                    </StatsContainer>
                 )}
             </Container>
         )
